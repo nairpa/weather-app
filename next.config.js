4ff0eee28635167381469128d1d40a14d2a11948
Nair Palacios <nairpalacios@live.com>
2023-05-06 17:44:31 -0300

    fix: modifes next js config base path

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/weather-app'
+  basePath: ''
 }
 
 module.exports = nextConfig
--- src/common/components/WeatherImage/WeatherImage.ts
+++ src/common/components/WeatherImage/WeatherImage.ts
@@ -5,82 +5,82 @@ export const WeatherImage = (weather: number) => {
            * Clear
            */
           case 0:
-            return "public/images/Clear.png"
+            return "/images/Clear.png"
           /**
            * Mainly clear
            */
           case 1:
           case 2:
           case 3:
-            return "public/images/LightCloud.png"
+            return "/images/LightCloud.png"
           /**
            * Fog
            */
           case 45:
           case 48:
-            return "public/images/HeavyCloud.png"
+            return "/images/HeavyCloud.png"
           /**
            * Drizzle
            */
           case 51:
           case 53:
           case 55:
-            return "public/images/LightRain.png"
+            return "/images/LightRain.png"
           /**
            * Freezing Drizzel
            */
           case 56:
           case 57:
-            return "public/images/Sleet.png"
+            return "/images/Sleet.png"
           /**
            * Rain
            */
           case 61:
           case 63:
           case 65:
-            return "public/images/HeavyRain.png"
+            return "/images/HeavyRain.png"
           /**
            * Freezing rain
            */
           case 66:
           case 67:
-            return "public/images/Sleet.png"
+            return "/images/Sleet.png"
           /**
            * Snow fall
            */
           case 71:
           case 73:
           case 75:
-            return "public/images/Snow.png"
+            return "/images/Snow.png"
           /**
            * Snow grains
            */
           case 77:
-            return "public/images/Hail.png"
+            return "/images/Hail.png"
           /**
            * Rain showers
            */
           case 80:
           case 81:
           case 82:
-            return "public/images/Shower.png"
+            return "/images/Shower.png"
           /**
            * Snow showers
            */
           case 85:
           case 86:
-            return "public/images/Snow.png"
+            return "/images/Snow.png"
           /**
            * Thunderstorm
            */
           case 95:
-            return "public/images/Thunderstorm.png"
+            return "/images/Thunderstorm.png"
           /**
            * Thunderstorm with hail
            */
           case 96:
           case 99:
-            return "public/images/Thunderstorm.png"
+            return "/images/Thunderstorm.png"
         }
       }
 
